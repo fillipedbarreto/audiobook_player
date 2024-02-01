b5c9dfef0889f1095775c25c54462644264efb99
Fillipe Barreto <fillipedbarreto@gmail.com>
2024-02-01 16:12:23 -0300

    minor changes (Final)

--- script.js
+++ script.js
@@ -12,26 +12,55 @@ let taTocado = 0;
 
 function tocarFaixa() {
     audioCapitulo.play();
-    botaoPlayPause.classList.remove(`bi-play-fill`);
-    botaoPlayPause.classList.add(`bi-pause-fill`);
     taTocado = 1;
 }
+
 function pausarFaixa() {
     audioCapitulo.pause();
-    botaoPlayPause.classList.add(`bi-play-fill`);
-    botaoPlayPause.classList.remove(`bi-pause-fill`);
     taTocado = 0;
 }
 
-function tocarOuPausar() {
+function pressPlayPause() {
     if (taTocado === 0) {
+        botaoPlayPause.classList.add(`bi-play-circle`);
+        botaoPlayPause.classList.remove(`bi-play-circle-fill`);
+    } else {
+        botaoPlayPause.classList.add(`bi-pause-circle`);
+        botaoPlayPause.classList.remove(`bi-pause-circle-fill`);
+    }
+}
+
+function releasePlayPause() {
+    if (taTocado === 0) {
+        botaoPlayPause.classList.remove(`bi-play-circle`);
+        botaoPlayPause.classList.add(`bi-pause-circle-fill`);
         tocarFaixa();
     } else {
+        botaoPlayPause.classList.remove(`bi-pause-circle`);
+        botaoPlayPause.classList.add(`bi-play-circle-fill`);
         pausarFaixa();
     }
 }
 
-function proximaFaixa() {
+function outPlayPause() {
+    if (taTocado === 0) {
+        botaoPlayPause.classList.remove(`bi-play-circle`);
+        botaoPlayPause.classList.add(`bi-play-circle-fill`);
+    } else {
+        botaoPlayPause.classList.remove(`bi-pause-circle`);
+        botaoPlayPause.classList.add(`bi-pause-circle-fill`);
+    }
+}
+
+function pressAvancar() {
+    botaoAvancar.classList.add(`bi-fast-forward-fill`);
+    botaoAvancar.classList.remove(`bi-fast-forward`);
+}
+
+function releaseAvancar() {
+    botaoAvancar.classList.remove(`bi-fast-forward-fill`);
+    botaoAvancar.classList.add(`bi-fast-forward`);
+    
     if (capituloAtual === 10) {
         capituloAtual = 1;
     } else {
@@ -43,7 +72,20 @@ function proximaFaixa() {
     tocarFaixa();
 }
 
-function voltarFaixa() {
+function outAvancar() {
+    botaoAvancar.classList.remove(`bi-fast-forward-fill`);
+    botaoAvancar.classList.add(`bi-fast-forward`);
+}
+
+function pressVoltar() {
+    botaoRetornar.classList.add(`bi-rewind-fill`);
+    botaoRetornar.classList.remove(`bi-rewind`);
+}
+
+function releaseVoltar() {
+    botaoRetornar.classList.remove(`bi-rewind-fill`);
+    botaoRetornar.classList.add(`bi-rewind`);
+
     if (capituloAtual === 1) {
         capituloAtual = 1;
     } else {
@@ -55,6 +97,22 @@ function voltarFaixa() {
     tocarFaixa();
 }
 
-botaoPlayPause.addEventListener(`click`, tocarOuPausar);
-botaoAvancar.addEventListener(`click`, proximaFaixa);
-botaoRetornar.addEventListener(`click`, voltarFaixa);
+function outVoltar() {
+    botaoRetornar.classList.remove(`bi-rewind-fill`);
+    botaoRetornar.classList.add(`bi-rewind`);
+}
+
+function changeIcon(iconAdd, iconRemove) {
+    botaoRetornar.classList.add(iconAdd);
+    botaoRetornar.classList.remove(iconRemove);
+}
+
+botaoPlayPause.addEventListener(`mousedown`, pressPlayPause);
+botaoPlayPause.addEventListener(`mouseup`, releasePlayPause);
+botaoPlayPause.addEventListener(`mouseout`, outPlayPause);
+botaoAvancar.addEventListener(`mousedown`, pressAvancar);
+botaoAvancar.addEventListener(`mouseup`, releaseAvancar);
+botaoAvancar.addEventListener(`mouseout`, outAvancar);
+botaoRetornar.addEventListener(`mousedown`, pressVoltar);
+botaoRetornar.addEventListener(`mouseup`, releaseVoltar);
+botaoRetornar.addEventListener(`mouseout`, outVoltar);
