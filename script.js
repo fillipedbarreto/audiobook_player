227d1034620b81ebc11da2f0a54f67fe9d79043a
Fillipe Barreto <fillipedbarreto@gmail.com>
2024-02-02 11:54:36 -0300

    Final project (Progress bar and time elapsed finished) - Consider changing imgs and songs

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ const botaoAvancar = document.getElementById(`proximo`);
 const botaoRetornar = document.getElementById(`anterior`);
 const audioCapitulo = document.getElementById(`audio-capitulo`);
 const tituloCapitulo = document.getElementById(`capitulo`);
+const progress = document.getElementById(`progress`);
+const elapsedTime = document.getElementById(`current-time`);
+const totalTime = document.getElementById(`total-time`);
 
 const numeroCapitulos = 10;
 let capituloAtual = 1;
 let taTocado = 0;
+totalTime.innerText =  `${formatSecondsAsTime(audioCapitulo.duration)}`;
+elapsedTime.innerText =  `${formatSecondsAsTime(audioCapitulo.currentTime)}`
+
 
 function tocarFaixa() {
     audioCapitulo.play();
@@ -105,6 +111,42 @@ function outVoltar() {
 function changeIcon(iconAdd, iconRemove) {
     botaoRetornar.classList.add(iconAdd);
     botaoRetornar.classList.remove(iconRemove);
+}
+
+audioCapitulo.onloadedmetadata = function () {
+    progress.max = audioCapitulo.duration;
+    totalTime.innerText =  `${formatSecondsAsTime(audioCapitulo.duration)}`;
+    progress.value = audioCapitulo.currentTime;
+}
+
+if (audioCapitulo.play()) {
+    setInterval(() => {
+        progress.value = audioCapitulo.currentTime;
+        elapsedTime.innerText = `${formatSecondsAsTime(audioCapitulo.currentTime)}`;
+    }, 500)
+}
+
+progress.onchange = function() {
+    audioCapitulo.play();
+    audioCapitulo.currentTime = progress.value;
+}
+
+function formatSecondsAsTime(secs) {
+    var hr  = Math.floor(secs / 3600);
+    var min = Math.floor((secs - (hr * 3600))/60);
+    var sec = Math.floor(secs - (hr * 3600) -  (min * 60));
+    if (min < 10){ 
+      min = "0" + min; 
+    }
+    if (sec < 10){ 
+      sec  = "0" + sec;
+    }
+    if (hr === 0) {
+        return min + ':' + sec;
+    } else {
+        return hr + ':' + min + ':' + sec;
+    }
+    
 }
 
 botaoPlayPause.addEventListener(`mousedown`, pressPlayPause);
